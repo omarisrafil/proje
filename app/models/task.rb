b6ad11e38e1f34bbea67badbccfca653989a7f40
omarisrafil <omar.israfil@bil.omu.edu.tr>
2017-06-28 15:06:14 +0300

    Add User components

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624124550) do
+ActiveRecord::Schema.define(version: 20170628084104) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
