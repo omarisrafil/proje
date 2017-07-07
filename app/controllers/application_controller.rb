a9df610ccf875a67e117a6d252fa369fbe1d98a9
omarisrafil <omar.israfil@bil.omu.edu.tr>
2017-07-07 12:24:56 +0300

    Edit CSS files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    if user_signed_in?
+  	  redirect_to tasks_path
+  	else
+  	  redirect_to new_user_session_path
+  	end
+  end
 end
